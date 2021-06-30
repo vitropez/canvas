ec3b64d6ca89f40be831b86489d05baf82b1e6fe
vitropez <64587299+vitropez@users.noreply.github.com>
2021-06-30 21:47:40 +0200

    dibujo caja

--- canvas.js
+++ canvas.js
@@ -1,20 +1,30 @@
 function iniciar() {
     var canvas = document.getElementById('canvas');
     var ctx = canvas.getContext('2d');
-    var alfa = 1;
 
-//cielo
-    let grd = ctx.createLinearGradient(0, 0, 0, 500);
-    grd.addColorStop(0, "rgba(0,225," + alfa + ")");
-    ctx.fillRect(0, 400, 1000, 100);
-
-//montaña
+    ctx.fillStyle = "#FF0000";
+    ctx.fillRect(400, 80, 200, 400);
     ctx.beginPath();
-    ctx.fillStyle = "rgba(155,0,255" + alfa + ")";
-    ctx.moveTo(0, 400);
-    ctx.lineTo(200, 100);
-    ctx.lineTo(400, 400);
-    ctx.fill();
+    ctx.moveTo(400,80);
+    ctx.lineTo(320,20);
+    ctx.lineWidth="3";
+    ctx.moveTo(400,480);
+    ctx.lineTo(320,400);
+    ctx.lineWidth="3";
+    ctx.moveTo(320,20);
+    ctx.lineTo(320,400);
+    ctx.lineWidth="3";
+    ctx.moveTo(600,80);
+    ctx.lineTo(500,20);
+    ctx.lineWidth="3";
+    ctx.moveTo(500,20);
+    ctx.lineTo(320,20);
+    ctx.lineWidth="3";
+    ctx.moveTo(400,80);
+    ctx.arc(440,80,40,3,2*Math.PI);
+    ctx.moveTo(500,80);
+    ctx.arc(540,80,40,3,2*Math.PI)
+    ctx.stroke();
 
 }
 window.addEventListener("load", iniciar, false);
