48182a8cce5c2d90cdf3ee1579d64eafbb35082f
vitropez <64587299+vitropez@users.noreply.github.com>
2021-07-01 21:12:00 +0200

    casi hecho el dibujo

--- canvas.js
+++ canvas.js
@@ -1,10 +1,14 @@
 function iniciar() {
     var canvas = document.getElementById('canvas');
     var ctx = canvas.getContext('2d');
-
-    ctx.fillStyle = "#FF0000";
-    ctx.fillRect(400, 80, 200, 400);
     ctx.beginPath();
+    ctx.fillStyle="rgba(0, 255, 0, 0.6)";
+    ctx.fillRect(400, 80, 200, 400);
+    ctx.strokeStyle = '#000099';
+    ctx.stroke();
+
+    ctx.beginPath();
+
     ctx.moveTo(400,80);
     ctx.lineTo(320,20);
     ctx.lineWidth="3";
@@ -24,7 +28,29 @@ function iniciar() {
     ctx.arc(440,80,40,3,2*Math.PI);
     ctx.moveTo(500,80);
     ctx.arc(540,80,40,3,2*Math.PI)
+
     ctx.stroke();
+
+    ctx.beginPath();
+    ctx.fillStyle="orange"
+    ctx.font="normal small-caps bold 50px verdana";
+    ctx.fillText("MILK",430,300);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.fillStyle="black";
+    ctx.arc(560,200,20,0,2*Math.PI);
+    ctx.moveTo(480,200);
+    ctx.arc(460,200,20,0,2*Math.PI);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.arc(510,350,40,3,2*Math.PI,true);
+    ctx.moveTo(480,200);
+    ctx.arc(460,200,20,0,2*Math.PI);
+    ctx.stroke();
+
+
 
 }
 window.addEventListener("load", iniciar, false);
